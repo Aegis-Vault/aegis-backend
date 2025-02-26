38178fbb0bcb786e7c78840e8c709ce701f107b9
Aegis-Vault <aegisvault1@gmail.com>
2025-02-25 23:34:51 -0500

    Added ExampleContract.sol to trigger security analysis

--- contracts/ExampleContract.sol
+++ contracts/ExampleContract.sol
@@ -1,10 +1,12 @@
+
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract ExampleContract {
     uint256 public value;
 
-    function setValue(uint256 _newValue) public {
-        value = _newValue;
+    function setValue(uint256 _value) public {
+        value = _value;
     }
 }
 
