1c46f5d13b891c8bdae96e0cba64d22494080b93
Aegis-Vault <aegisvault1@gmail.com>
2025-02-25 23:40:36 -0500

    Added NewTest.sol to trigger security analysis

---
+++ contracts/NewTest.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+contract NewTest {
+    uint256 public value;
+    function setValue(uint256 _value) public {
+        value = _value;
+    }
+}
