628636450a38339936d88929576e7f8d25ab345f
Aegis-Vault <aegisvault1@gmail.com>
2025-02-26 13:18:11 -0500

    Fix: Updated Solidity version and naming convention

--- contracts/Test.sol
+++ contracts/Test.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.28;
 contract Test {
     uint256 public value;
-    function setValue(uint256 _value) public {
-        value = _value;
-    }
+	function setValue(uint256 newValue) public {
+    		value = newValue;
 }
+
